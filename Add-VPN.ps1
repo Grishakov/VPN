7eb8cd91d3fc69cd04df3ef18ffc40b5662b1de2
Andrey Grishakov <115949582+Grishakov@users.noreply.github.com>
2024-07-03 01:06:48 +0300

    Add files via upload

---
+++ Add-VPN.ps1
@@ -0,0 +1,24 @@
+﻿$ConnectionName=Read-Host "Enter connection name"
+$ServerAddress=Read-Host "Enter VPN Server IP"
+$L2tpPsk=Read-Host "Enter ipsec pre-shared key"
+$DestinationPrefix=Read-Host "Enter destination network"
+$DNSSuffix=Read-Host "Enter dns suffix"
+$DnsIPAddress=Read-Host "Enter dns server"
+
+Add-VpnConnection -Name "$ConnectionName" `
+    -ServerAddress "$ServerAddress" `
+    -TunnelType "L2tp" `
+    -L2tpPsk "$L2tpPsk" `
+    -Force `
+    -EncryptionLevel "Required" `
+    -AuthenticationMethod MSChapv2 `
+    -SplitTunneling `
+    -RememberCredential `
+    -PassThru
+
+#   -AllUserConnection $true
+Add-VpnConnectionRoute -ConnectionName "$ConnectionName" -DestinationPrefix $DestinationPrefix –PassThru
+
+Add-VpnConnectionTriggerDnsConfiguration -ConnectionName $ConnectionName -DnsSuffix $DNSSuffix -DnsIPAddress $DnsIPAddress -PassThru
+
+Read-Host -Prompt "Press Enter to exit"
